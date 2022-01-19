25abe76715b25f37134ba46bf30a8df7c89bf53a
jjongwa <troas96@naver.com>
2022-01-19 10:44:18 +0900

    220119

---
+++ 2003.py
@@ -0,0 +1,39 @@
+import sys
+#input = sys.stdin.readline()
+N, M = map(int,input().split())
+A_list = list(map(int,sys.stdin.readline().split()))
+#print(A_list)
+ans = 0
+
+for i in range(1,N+1):
+    p_left = 0 
+    p_right = p_left + i-1  # pointer
+    tmp = 0
+    for j in range(p_left,p_right+1):
+        tmp += A_list[j]
+    while p_right < len(A_list):
+        if p_left != 0:
+            tmp -= A_list[p_left-1]
+            tmp += A_list[p_right]
+        if tmp == M:
+            ans += 1    
+        p_left += 1
+        p_right += 1
+
+print(ans)    
+
+
+'''
+for i in range(1,N+1):
+    win_size = i
+    window = deque()
+    for j in range(N):
+        if len(window) < win_size:
+            window.append(A_list[j])
+        if len(window) == win_size:
+            if sum(window) == M:
+                ans +=1
+            window.popleft()
+'''
+
+
