3b5d0295078f06a8b695065d2d2268f84b849bb4
jjongwa <troas96@naver.com>
2022-05-22 23:46:41 +0900

    220522

--- 1562-계단수.py
+++ 1562-계단수.py
@@ -4,10 +4,10 @@ sys.stdin = open('input.txt', 'r')
 
 
 
-N, X = map(int,input().split())
-A = list(map(int,input().split()))
-
-for i in A:
-    if i < X:
-        print(i,end=' ')        
-
+N = int(input())
+for _ in range(N):
+    R, S = map(str,input().split())
+    for i in range(len(S)):
+        for j in range(int(R)):
+            print(S[i],end='')
+    print()
