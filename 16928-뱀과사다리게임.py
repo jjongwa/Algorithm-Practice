fcb7a4486404ae25f48ff772e6ef2cb681febe3e
jjongwa <troas96@naver.com>
2022-05-31 10:09:10 +0900

    220531

--- 16928-뱀과사다리게임.py
+++ 16928-뱀과사다리게임.py
@@ -4,29 +4,29 @@ sys.stdin = open('input.txt', 'r')
 from collections import deque
 
 N, M = map(int, input().split())
-warp = []
+warp = {}
 for _ in range(N+M):
     a, b = map(int, input().split())
-    warp.append((a,b))
+    warp[a] = b
 
 visited = [0 for _ in range(101)]
+board = [0 for _ in range(101)]
 dq = deque([1])
 
 
 while dq:
     now = dq.popleft()   
     for i in range(1, 7):
-        if now + 1 <= 100 and visited[now+1] == 0:
+        if now + i <= 100 and visited[now+i] == 0:
             nxt = now + i
-            visited[nxt] = visited[now]+ 1
             if nxt in warp:
-                visited[warp[nxt]] = visited[now] + 1
-                dq.append(warp[nxt])
-            else:
+                nxt = warp[nxt]
+            if visited[nxt] == 0:
+                visited[nxt] = 1
+                board[nxt] = board[now]+ 1
                 dq.append(nxt)
-
-    if visited[100]:
-        print(visited[100])
+    if now == 100:
+        print(board[100])
         break            
 
 
