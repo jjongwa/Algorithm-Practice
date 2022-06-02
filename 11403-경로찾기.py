82eb2f77286be9674607ee459f8070114dd48289
jjongwa <troas96@naver.com>
2022-06-02 14:46:26 +0900

    220602

---
+++ 11403-경로찾기.py
@@ -0,0 +1,34 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+
+#from collections import deque
+N = int(input())
+arr = [list(map(int, input().split()))for _ in range(N)] 
+ans = [[0]*N for _ in range(N)] 
+
+
+
+stack = []
+for i in range(N):
+    for j in range(N):
+        if arr[i][j] == 1:
+            ans[i][j] = 1
+            stack.append([i,j])
+    while stack:
+        start, now = stack.pop()
+        for nxt in range(N):
+            if ans[start][nxt] == 0 and arr[now][nxt] == 1:
+                ans[start][nxt] =1
+                stack.append([start, nxt])
+        
+for i in range(N):
+    for j in range(N):
+        print(ans[i][j], end=' ')
+    print()
+
+        
+
+
+
+
+
