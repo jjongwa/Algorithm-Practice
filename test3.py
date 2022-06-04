b7ac233e6d1f7a71a23f89e6dd8b18c2dbee9353
jjongwa <troas96@naver.com>
2022-06-04 23:21:13 +0900

    220604

--- test3.py
+++ test3.py
@@ -1,4 +1,13 @@
 import sys
 sys.stdin = open('input.txt', 'r')
-l = list(map(str,input().split()))
-print(len(l))
+a, b = map(str, input().split())
+
+na = int(a[2])*100 + int(a[1])*10 + int(a[0])
+nb = int(b[2])*100 + int(b[1])*10 + int(b[0])
+print(na,nb)
+if int(na) < int(nb):
+    for i in range(2,-1,-1):
+        print(b[i],end='')
+else:
+    for i in range(2,-1,-1):
+        print(a[i],end='')
