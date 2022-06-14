c433c1f13bea6d6b9cbfc1043aa19a5600820efe
jjongwa <troas96@naver.com>
2022-06-14 09:54:57 +0900

    220614

--- 1966-프린터큐.py
+++ 1966-프린터큐.py
@@ -1,2 +1,27 @@
 import sys
 sys.stdin = open('input.txt', 'r')
+
+from collections import deque
+N = int(input())
+for _ in range(N):
+    N, M = map(int, input().split())
+    dq = deque(map(int, input().split()))
+    target = dq[M]
+    cnt = 1
+    while True:
+        if dq[0] >= max(dq):
+            if M == 0:
+                print(cnt)
+                break
+            dq.popleft()
+            M -= 1
+            cnt += 1
+        else:
+            if M == 0:
+                M = len(dq)-1
+            else:
+                M -= 1
+            tmp = dq.popleft()
+            dq.append(tmp)
+            
+            
