51a60c5943567ef59a2898ab74aaa331c6d986e3
jjongwa <troas96@naver.com>
2022-06-25 23:56:31 +0900

    220624

--- 2744-대소문자바꾸기.py
+++ 2744-대소문자바꾸기.py
@@ -1,8 +1,10 @@
 import sys
 sys.stdin = open('input.txt', 'r')
-n = int(input())
-fi = [0  for _ in range(21)]
-fi[1] = 1
-for i in range(2,21):
-    fi[i] = fi[i-1]+fi[i-2]
-print(fi[n])
+
+w =input()
+
+for i in w:
+    if i.isupper():
+        print(i.lower(), end = "")
+    else:
+        print(i.upper(), end = "")
