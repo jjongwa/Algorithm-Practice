2e35cfe1c118c53186ccf96c3b984b16e763433a
jjongwa <troas96@naver.com>
2023-01-01 23:44:44 +0900

    230101

---
+++ 2377-Pottery.py
@@ -0,0 +1,24 @@
+print(" _________")
+print(" \_     _/")
+print("   \   /")
+print("    | |")
+print("   /   \\")
+print("  /     \\")
+print(" |       |")
+print("/---------\\")
+print("| \-/ \-/ |")
+print("\---------/")
+print(" \_______/")
+
+
+# print" _________"
+# print" \_     _/"
+# print"   \   /"
+# print"    | |"
+# print"   /   \"
+# print"  /     \"
+# print" |       |"
+# print"/---------\"
+# print"| \-/ \-/ |"
+# print"\---------/"
+# print" \_______/"
