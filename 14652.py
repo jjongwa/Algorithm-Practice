3f4eda5b020f906115bb3b12f52003420a69ef20
jjongwa <troas96@naver.com>
2023-02-05 23:23:53 +0900

    230205

---
+++ 14652.py
@@ -0,0 +1,11 @@
+n, m, k = map(int, input().split())
+n, m, k = map(int, input().split())
+print(k//m, k%m)
+# an, am = 0, 0
+#
+# for _ in range(k-1):
+#     am += 1
+#     if am == m-1:
+#         am = 0
+#         an += 1
+# print(an, am)
